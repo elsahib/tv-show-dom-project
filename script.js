3628052d91375ba12537e9ed3334e894d529835e
elsahib10@gmail.com <elsahib10@gmail.com>
2020-06-21 23:31:47 +0100

    Level 100 - Minimal features

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 //You can edit ALL of the code here
+const rootElem = document.getElementById("root");
+
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  for (let i = 0; i < episodeList.length; i++) {
+    let episodeContainer = document.createElement("div"),
+        episodeName = document.createElement("h2"),
+        episodeImg = document.createElement("img");
+    rootElem.classList.add("container");
+    episodeContainer.classList.add("episode");
+    episodeName.classList.add("name")
+    episodeName.innerText = episodeList[i].name + " - " + "S" + episodeList[i].season.toString().padStart(2,"0") + "E"+
+    episodeList[i].number.toString().padStart(2,"0");
+    episodeImg.src = episodeList[i].image.medium;
+    episodeContainer.append(episodeName,episodeImg);
+    episodeContainer.innerHTML += episodeList[i].summary;
+    rootElem.append(episodeContainer);
+  }
+  
 }
 
 window.onload = setup;
