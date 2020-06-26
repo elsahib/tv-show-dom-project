0887dcd164a4d9682e1ea851f7b43e3004e84129
elsahib10@gmail.com <elsahib10@gmail.com>
2020-06-26 12:50:47 +0100

    minor change _foreach instead of for loop_

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//You can edit ALL of the code here
 // Global variables
 const rootElem = document.getElementById("root");
 let searchBox = document.getElementById("search");
@@ -7,10 +6,9 @@ const selectEpisode = document.getElementById("allEpisodes");
 // first function gets executed as soon as the page is fully loaded.
 function setup() {
   makePageForEpisodes(allEpisodes);
-  // calling listEpisodes to populate the dropdown select input with options.
+
   listEpisodes(allEpisodes);
-  // adding an event Listener to create a live search feature, using filter method through the array of episodes and utilising the
-  // string includes method inside a custom made function named search
+
   searchBox.addEventListener("input", () => {
     let resultNumber = document.getElementById("resultNumber");
     let results = allEpisodes.filter(containsSearchTerm);
@@ -20,7 +18,7 @@ function setup() {
       resultNumber.innerText = "";
     }
   });
-  // Adding event for the dropdown selector to show only selected episode.
+
   selectEpisode.addEventListener("change", () => {
     if (selectEpisode.value === "novalue") {
       makePageForEpisodes(allEpisodes);
@@ -32,14 +30,14 @@ function setup() {
     }
   });
 }
-// function to display episodes inside the root element
+
 function makePageForEpisodes(episodeList) {
-  rootElem.innerHTML = ""; // Clearing the main container
-  for (let i = 0; i < episodeList.length; i++) {
-    rootElem.append(makeContainer(episodeList[i]));
-  }
+  rootElem.innerHTML = "";
+  episodeList.forEach((episode) => {
+    rootElem.append(makeContainer(episode));
+  });
 }
-// function to create the shadow container element and add it's children
+
 function makeShadow(episode) {
   let episodeShadow = document.createElement("div");
   episodeShadow.classList.add("card", "mb-4", "shadow-sm");
@@ -50,7 +48,7 @@ function makeShadow(episode) {
   );
   return episodeShadow;
 }
-// function to create the episode container element and add it's children
+
 function makeContainer(episode) {
   let episodeContainer = document.createElement("div");
   episodeContainer.classList.add("col-md-4");
@@ -58,7 +56,6 @@ function makeContainer(episode) {
   return episodeContainer;
 }
 
-// function to create the title element and add it's children
 function makeTitle(episode) {
   let episodeTitle = document.createElement("div");
   episodeTitle.classList.add(
@@ -70,14 +67,14 @@ function makeTitle(episode) {
   episodeTitle.append(makeName(episode), makeNameCode(episode));
   return episodeTitle;
 }
-// function to create the name element and fill it with data
+
 function makeName(episode) {
   let episodeName = document.createElement("div");
   episodeName.classList.add("btn-group", "name");
   episodeName.innerText = episode.name;
   return episodeName;
 }
-// function to create the name code element and fill it with data
+
 function makeNameCode(episode) {
   let episodeNameCode = document.createElement("mediuem");
   episodeNameCode.classList.add("episodeNameCode");
@@ -86,7 +83,6 @@ function makeNameCode(episode) {
   return episodeNameCode;
 }
 
-// function that generates a code for each episode
 function makeCode(episode) {
   return (
     "S" +
@@ -96,14 +92,13 @@ function makeCode(episode) {
   );
 }
 
-// function to create the image element
 function makeImage(episode) {
   let episodeImg = document.createElement("img");
   episodeImg.classList.add("bd-placeholder-img", "card-img-top");
   episodeImg.src = episode.image.medium;
   return episodeImg;
 }
-// function to create the body element and fill it with data
+
 function makeBody(episode) {
   let episodeBody = document.createElement("div");
   episodeBody.classList.add("card-body");
@@ -111,7 +106,6 @@ function makeBody(episode) {
   return episodeBody;
 }
 
-// function to return episode code and name and create the option element and fill it with data
 function listEpisodes(episodes) {
   episodes.forEach((episode) => {
     let dropDownOption = document.createElement("option");
@@ -120,7 +114,7 @@ function listEpisodes(episodes) {
     selectEpisode.appendChild(dropDownOption);
   });
 }
-// this will return true or false based on user input.
+
 function containsSearchTerm(episode) {
   return (
     episode.summary.toLowerCase().includes(searchBox.value.toLowerCase()) ||
